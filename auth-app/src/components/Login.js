56f8befdbdf7bb52b339c84cd972908fc9cc0bb4
shubamWanclouds <sdadhwal@wanclouds.net>
2021-09-21 13:04:34 +0530

    login UI impovement

--- auth-app/src/components/Login.js
+++ auth-app/src/components/Login.js
@@ -1,5 +1,7 @@
 import React, { useState, useRef } from 'react'
 import { Link, useHistory } from 'react-router-dom'
+import user from '../assets/avatar.svg'
+import './Login.css'
 
 function Login() {
   let history = useHistory()
@@ -8,15 +10,14 @@ function Login() {
   const [loginPasswd, setLoginPasswd] = useState('')
   const [loginError, setLoginError] = useState('')
 
-  const rememberMeRef = useRef(null)
+  const rembrMeRef = useRef(true)
 
   const handleLogin = (event) => {
     event.preventDefault()
+    console.log(rembrMeRef.current.value)
     let userData = JSON.parse(localStorage.getItem("userData"))
-    console.log(userData)
     if((loginEmail === userData.email) && (loginPasswd === userData.passwd))
       {
-        console.log(rememberMeRef.current.value)
         userData["isLoggedIn"] = true
         localStorage.setItem('userData', JSON.stringify(userData))
         history.push('/')
@@ -26,26 +27,23 @@ function Login() {
   }
 
   return (
-    <div className="container-sm mt-5">
-      <form onSubmit={handleLogin}>
-        <h1 className="text-center">LOGIN</h1>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">Email address</label>
-          <input type="email" className="form-control" id="email" value={loginEmail} onChange={e=>setLoginEmail(e.target.value)}/>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">Password</label>
-          <input type="password" className="form-control" id="password" value={loginPasswd} onChange={e=>setLoginPasswd(e.target.value)}/>
-        </div>
-        <div className="mb-3 form-check">
-          <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-          <label className="form-check-label" htmlFor="exampleCheck1" ref={rememberMeRef}>Remember Me</label>
-        </div>
+    <div class="form">
+      <form onSubmit={handleLogin} >
+      <div class="imgcontainer">
+        <img src={user} alt="icon" class="icon"/>
+        <h2>Sign In</h2>
+      </div>
+      <div class="container">
+        <label for="email"><b>Email</b></label>
+        <input type="text" placeholder="Enter email" name="email" id="email" value={loginEmail} onChange={e=>setLoginEmail(e.target.value)} required/>
+        <label for="passwd"><b>Password</b></label>
+        <input type="password" placeholder="Enter password" name="passwd" id="passwd" value={loginPasswd} onChange={e=>setLoginPasswd(e.target.value)} required/>
+        <button id="submit" className="btn btn-success">Login</button>
+        <Link id="emailHelp" className="form-text mt-5" to="/Signup" style={{fontSize:16, marginTop:5}}>New user?</Link>
         <div style={{color:'red',fontSize:15,margin:5}}>{loginError}</div>
-        <button type="submit" className="btn btn-primary">Login</button> &nbsp;&nbsp; <br />
-        <Link id="emailHelp" className="form-text mt-5" to="/Signup">New user?</Link>
+      </div>
       </form>
-  </div>
+    </div>
   )
 }
 
